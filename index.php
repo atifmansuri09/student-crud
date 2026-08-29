8b6576a35876f7fad6940be115495e62f253f347
atifmansuri09 <mansuriatif46@gmail.com>
2026-08-29 21:22:49 +0530

    Created Home and About Half-Half

--- index.php
+++ index.php
@@ -1,9 +1,67 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Student</title>
-    </head>
-    <body>
-        <h1>Index Page of Student Crud</h1>
-    </body>
+
+<head>
+    <title>Index Page</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">LOGO</a>
+            <a class="navbar-brand" href="login.php">Login</a>
+            <!-- <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form> -->
+        </div>
+    </nav>
+
+    <!-- HERO SECTION -->
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="10000">
+                <div class="hero-img1" style="z-index: 5;">
+                    <!-- <img src="hero-img3.jpg" class="d-block w-100" alt="..."> -->
+                    <p class="Header ms-5 text-light fw-bold">WELCOME TO</p>
+                    <p class="Header ms-5 text-light fw-bold">HOGWARTZ</p>
+                    <p class="Body text-light"><i>The School of Witchcraft and Wizardry</i></p>
+                    <p class="Body text-light"><i>Manage your magical academic<br> journey with ease.</i></p>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+                <div class="hero-img1">
+                    <p class="text-light fw-bold">WELCOME TO</p>
+                    <p class="text-light fw-bold">HOGWARTZ</p>
+                    <p class="text-light "><i>The School of Witchcraft and Wizardry</i></p>
+                    <p class="text-light "><i>Manage your magical academic journey with ease.</i></p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="hero-img1">
+                    <p class="text-light fw-bold">WELCOME TO</p>
+                    <p class="text-light fw-bold">HOGWARTZ</p>
+                    <p class="text-light "><i>The School of Witchcraft and Wizardry</i></p>
+                    <p class="text-light "><i>Manage your magical academic journey with ease.</i></p>
+                </div>
+                <!-- <img src="..." class="d-block w-100" alt="..."> -->
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <script src="bootstrap.bundle.min.js"></script>
+</body>
+
 </html>
